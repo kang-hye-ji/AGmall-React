070cad64b16099eb12643fe65352e9357489f4a1
KHJ <kanghj9480@naver.com>
2020-08-30 16:23:15 +0900

    change access-control...

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,7 +6,7 @@ router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     //res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    //res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 router.post('/lists', (req, res)=>{
--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,7 @@ router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     //res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    //res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 router.post('/register', (req, res)=>{
