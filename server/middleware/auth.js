4c641580d80c90638e46630ab3928a9d291aee05
KHJ <kanghj9480@naver.com>
2020-08-30 22:23:24 +0900

    auth modify

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,34 @@
 const {User} =require('../models/user');
 
 let auth=(req, res, next)=>{
-    let token= req.session.w_auth;/* res.cookie.w_auth; */
+    User.findByToken(req.session.w_auth, (err,user)=>{
+        if(err) console.log(err);
+        if(!user){
+            return res.json({
+                isAuth:false,
+                error:true
+            })
+        }
+        req.token=req.session.w_auth;
+        req.user=user;
+        next();
+    })
+}
+/* let auth=(req, res, next)=>{
+    let token= req.session.w_auth;
     console.log(req.session.w_auth);
     User.findByToken(token, (err,user)=>{
         if(err) console.log(err);
         if(!user){
             return res.json({
                 isAuth:false,
-                error:true,
-                token:null
+                error:true
             })
         }
         req.token=token;
         req.user=user;
         next();
     })
-}
+} */
 
 module.exports={auth};
