bf8cdfb3cccd8cff68be3df1af8eb8d1286dc3ea
KHJ <kanghj9480@naver.com>
2020-08-31 16:59:53 +0900

    change from a to button

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -60,7 +60,8 @@ function Header(props) {
                                 </li>
                                 <li><a href="!#" onClick={e=>{e.preventDefault();}} title="" target="_self">고객센터</a></li>
                                 <li>
-                                    <a href="!#" onClick={logoutHandler}>로그아웃</a>
+                                    {/* <a href="!#" onClick={logoutHandler}>로그아웃</a> */}
+                                    <button onClick={logoutHandler}>로그아웃</button>
                                 </li>
                             </ul>
                             :
