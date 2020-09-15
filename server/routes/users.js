25babc7c9f612ae2ff874f46c4b6bf67d48b10c2
KHJ <kanghj9480@naver.com>
2020-09-15 16:55:38 +0900

    re

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,7 +11,7 @@ router.use(function(req, res, next) {
     //res.header("Access-Control-Allow-Headers", "*");
     //res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
-    /* res.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=None") */
+    res.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=None")
     next();
 });
 router.post('/register', (req, res)=>{
